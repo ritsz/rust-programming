232b01ba78ffe3bde5e82db49c91b5b56cba93a7
ritsz <r.ranjan789@gmail.com>
2017-03-19 18:08:37 +0530

    Variable bindings

---
+++ learning/src/main.rs
@@ -0,0 +1,56 @@
+
+/*
+   0. Arguments during sending and function parameters must match.
+      That is how move and borrows are decided. Variables can be
+      mutable or immutable. Immutable variables can be moved and 
+      borrowed immutably.
+   1. Immutable variables CAN'T have mutable borrows
+   2. Immutable variables CAN have mutable ownership moves
+   3. Mutable variables can have mutable borrows.
+   4. Mutable variables can have mutable moves.
+   5. Borrowed variables can be borrowed again.
+   6. Borrowed variables can't be moved by borrower. Moves can only happen from original owner.
+ */
+fn main() {
+    let (string, z)   = ("Hello World".to_string(), "Original String".to_string());
+    /*
+        mutable_borrow(&mut string, 1);
+        error: cannot borrow immutable local variable `string` as mutable
+     */
+    mutable_move(string, 2);
+    let mut mut_string:String = "Mutable String".to_string();
+    mutable_borrow(&mut mut_string, 3);
+    mutable_move(mut_string, 4);
+
+    let borrow_string:&String = &z;
+    immutable_borrow(borrow_string, 5);
+    println!("6. {}", immutable_borrow_move(borrow_string, 6));
+
+}
+
+fn mutable_borrow(x: &mut String, index: u32) {
+    println!("{}. {}", index, x);
+}
+
+fn mutable_move (mut x: String, index: u32) {
+    x = "FOO".to_string();
+    println!("{}. {}", index, x);
+}
+
+fn immutable_borrow(x: &String, index: u32) {
+    println!("{}. {}", index, x);
+}
+
+fn immutable_move(x:String, index: u32) {
+    println!("{}. {}", index, x);
+}
+
+fn immutable_borrow_move(x: &String, index: u32) -> String {
+    /*
+        *x
+        error[E0507]: cannot move out of borrowed content
+        x was borrowed, you don't own it. You can't move it to someone else.
+     */
+    "Dummy".to_string()
+}
+
