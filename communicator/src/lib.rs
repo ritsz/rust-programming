b8c74d5e299e63562fb0b471a50e07594e7c1f97
ritsz <r.ranjan789@gmail.com>
2017-04-09 20:25:25 +0530

    Layered module. gettit?

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -5,6 +5,7 @@ use pnet::datalink::{self, NetworkInterface};
 /* Define a module here. Implementation will look for a file named server.rs */
 mod server;
 
+/* Different module, looks for a folder named layer and a file layer/mod.rs */
 mod layer;
 
 /* Different modules can have same names. */
