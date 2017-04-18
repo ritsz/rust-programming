4445e96d6f1073f552800dbb45ff2f77f2996fa8
ritsz <r.ranjan789@gmail.com>
2017-04-19 00:37:05 +0530

    Lifetime of references

--- learning/src/main.rs
+++ learning/src/main.rs
@@ -11,6 +11,8 @@
    5. Borrowed variables can be borrowed again.
    6. Borrowed variables can't be moved by borrower. Moves can only happen from original owner.
    7. Borrowed varibles can be further borrowed.
+   8. Single reference borrow and return doesn't need reference lifetimes
+   9. Multiple refernces borrow require a lifetime parameter.
  */
 fn main() {
     let (string, z)   = ("Hello World".to_string(), "Original String".to_string());
@@ -30,6 +32,12 @@ fn main() {
     let mut mut_string = "THIS IS MUTABLE".to_string();
     mutable_borrow_borrow(&mut mut_string, 7);
 
+    borrow_single_ref(&mut_string);
+    
+    mut_string = "THIS IS FOR LIFETIME CHECK".to_string();
+    borrow_two_ref(&mut_string, &9);
+
+    test_nested_lifetimes();
 }
 
 fn mutable_borrow(x: &mut String, index: u32) {
@@ -61,4 +69,45 @@ fn immutable_borrow_move(x: &String, index: u32) -> String {
 fn mutable_borrow_borrow(x: &mut String, index: u32) -> &mut String {
     println!("{}. {}", index, x);
     x
+}
+
+/* Since these refernces will have the same lifetime, we don't need a lifetime parameter. */
+fn borrow_single_ref(x: &String) -> &String {
+    let some = "Hello".to_string();
+    println!("8. {}", x);
+    x
+}
+
+fn borrow_two_ref<'a, 'b>(x: &'a String, index: &'b u32) -> &'a String {
+    println!("{}. {}", index, x);
+    x
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn test_nested_lifetimes() {
+    let x = "Longish string";
+
+    {
+        let y = "Small";
+        let result = longest(x,y);
+        println!("10. {:?}. /*Generic lifetimes get set to the smallest lifetime*/", result);
+    }
+
+    /*
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+     //string2 doesn't live long enough. It could still be borrowed here but is out of scope
+    println!("The longest string is {}", result);
+    */
 }
