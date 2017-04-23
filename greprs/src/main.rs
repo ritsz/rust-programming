2297d84e44a15707cfd12ba8595345cc5e1274f0
ritsz <r.ranjan789@gmail.com>
2017-04-24 01:03:27 +0530

    Handle flag through struct. use new and Result

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -1,6 +1,33 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
+/* Handle the configs*/
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+
+       /* Instead of panic we can send reasonable Result messages */
+        if args.len() < 3
+        {
+            return Err("not enough Args");
+        }
+
+        /* Create a clone of the values so that they can be used as values in Config */
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config {
+            query: query,
+            filename: filename,
+        })
+    }
+}
+
 
 fn main() {
     /*
@@ -15,9 +42,14 @@ fn main() {
        Will have to use ref or &.
        let filename = args[1];
      */
-    let ref filename = args[1];
 
-    let mut fd = File::open(filename).expect("File not found");
+    /* Unwrap of handle the error using closure */
+    let config = Config::new(&args).unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {}", err);
+            process::exit(1);
+            });
+    
+    let mut fd = File::open(config.filename).expect("File not found");
     let mut contents = String::new();
     fd.read_to_string(&mut contents).expect("something went wrong reading the file");
 
