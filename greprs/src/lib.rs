79758307e9bb551da7e6fad782d2708f0794a7e9
ritsz <r.ranjan789@gmail.com>
2017-04-24 14:04:44 +0530

    Make a case insensetive search
    1. Add getter methods and make variables private
    2. Pattern matching to handle both case matches

--- greprs/src/lib.rs
+++ greprs/src/lib.rs
@@ -1,12 +1,14 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
+use std::env;
 
 /* Handle the configs*/
 #[derive(Debug)]
 pub struct Config {
-    pub query: String,
-    pub filename: String,
+    query: String,
+    filename: String,
+    case_sensetive: bool,
 }
 
 impl Config {
@@ -21,11 +23,27 @@ impl Config {
         /* Create a clone of the values so that they can be used as values in Config */
         let query = args[2].clone();
         let filename = args[1].clone();
+        let case_sensetive = false;
+
+        let case_sensetive = env::var("CASE_SENSETIVE").is_err();
 
         Ok(Config {
             query: query,
             filename: filename,
+            case_sensetive: case_sensetive,
         })
+    }
+
+    pub fn query_string(&self) -> &str {
+        &self.query
+    }
+
+    pub fn filename_string(&self) -> &str {
+        &self.filename
+    }
+
+    pub fn is_case_sensetive(&self) -> bool {
+        self.case_sensetive
     }
 }
 
@@ -36,13 +54,25 @@ impl Config {
    Error depends on runtime here. We should use Object traits.
  */
 pub fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename)?;
+    let mut f = File::open(config.filename_string())?;
     let mut contents = String::new();
     f. read_to_string(&mut contents)?;
 
-    for line in search(&config.query, &contents) {
-        println!("{}", line);
+    match config.is_case_sensetive() {
+        true => {
+            for line in search(&config.query_string(), &contents) {
+                println!("{}", line);
+            }
+        }
+        false => {
+            for line in search(&config.query_string().to_lowercase(), &contents.to_lowercase()) {
+                println!("{:?}", line);
+            }
+        }
+
     }
+
+
 
     Ok(())
 }
--- greprs/target/debug/greprs.d
+++ greprs/target/debug/greprs.d
@@ -1,1 +1,1 @@
-/home/ritsz/Programming/Rust/greprs/target/debug/greprs: /home/ritsz/Programming/Rust/greprs/src/lib.rs /home/ritsz/Programming/Rust/greprs/src/main.rs
+/home/ritsz/Programming/Rust/greprs/target/debug/greprs: /home/ritsz/Programming/Rust/greprs/src/main.rs /home/ritsz/Programming/Rust/greprs/src/lib.rs
